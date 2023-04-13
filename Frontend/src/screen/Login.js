bca79ca8e2c0dc4d4f50828a8d912c1260ed3f69
dharmesh2288 <107540589+dharmesh2288@users.noreply.github.com>
2023-04-13 22:51:44 +0530

    Update Login.js

--- Frontend/src/screen/Login.js
+++ Frontend/src/screen/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:5000/api/loginuser", {
+        const response = await fetch("https://gofoodbackend-hf66.onrender.com/api/loginuser", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
